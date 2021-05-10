266332610728e27733f6f2f1f0de011d770ddac7
iliagrishakov <i.grishakov@bankingon.io>
2021-05-10 15:41:44 +0300

    added search

--- lib/ui/screens/notes/notes_list_screen.dart
+++ lib/ui/screens/notes/notes_list_screen.dart
@@ -19,145 +19,169 @@ class NotesList extends StatefulWidget {
 
 class _NotesListState extends State<NotesList> {
   NotesListCubit _notesListCubit;
+  TextEditingController _searchTextController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+    _searchTextController.addListener(() => setState(() {}));
     _notesListCubit = NotesListCubit(RepositoryProvider.of<NotesRepository>(context));
     _notesListCubit.loadNotes();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Notes'),
-        centerTitle: true,
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: (selected) {
-              if (selected == 'LogOut') {
-                Navigator.of(context)
-                    .pushReplacement(MaterialPageRoute(builder: (context) => AuthScreen()));
-                RepositoryProvider.of<NotesRepository>(context).clearNotes();
-              } else if (selected == 'User') {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => UserScreen(), fullscreenDialog: true));
-              }
-            },
-            itemBuilder: (BuildContext itemContext) {
-              return [
-                PopupMenuItem<String>(value: 'LogOut', child: Text('LogOut')),
-                PopupMenuItem<String>(value: 'User', child: Text('Edit User')),
-              ];
-            },
-          ),
-        ],
-      ),
-      body: BlocConsumer<NotesListCubit, NotesListState>(
-        cubit: _notesListCubit,
-        listener: (newContext, state) {
-          if (state is NotesListFailedState) {
-            showDialog(
-              context: context,
-              builder: (dialogContext) {
-                return AlertDialog(
-                  title: Text('Error'),
-                  content: Container(
-                    child: Text(state.error),
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(dialogContext).pop();
-                      },
-                      child: Text('OK'),
-                    )
-                  ],
-                );
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Notes'),
+          centerTitle: true,
+          actions: [
+            PopupMenuButton<String>(
+              onSelected: (selected) {
+                if (selected == 'LogOut') {
+                  Navigator.of(context)
+                      .pushReplacement(MaterialPageRoute(builder: (context) => AuthScreen()));
+                  RepositoryProvider.of<NotesRepository>(context).clearNotes();
+                } else if (selected == 'User') {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => UserScreen(), fullscreenDialog: true));
+                }
               },
-            );
-          }
-        },
-        builder: (newContext, state) {
-          if (state is NotesListLoadingState || state is NotesListInitialState) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is NotesListFailedState) {
-            return _buildTryAgain();
-          } else if (state is NotesListLoadedState) {
-            return ListView.separated(
-              itemBuilder: (BuildContext itemContext, int index) {
-                final note = state.notes[index];
-                return NoteItem(
-                  title: note.title,
-                  date: note.dateCreated,
-                  onTap: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => NoteScreen(
-                        note: note,
-                        onEditingFinished: (String body, String title) {
-                          Navigator.of(context).pop();
-                          final newNote = Note(body, title, note.id);
-                          _notesListCubit.editNote(newNote);
-                        },
-                      ),
-                      fullscreenDialog: true,
-                    ),
-                  ),
-                  onLongTap: () => showDialog(
-                    context: context,
-                    builder: (dialogContext) => AlertDialog(
-                      title: Text('Deleting'),
-                      content: Text('Do u want delete this note?'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(dialogContext).pop();
-                            _notesListCubit.deleteNote(note.id);
-                          },
-                          child: Text('Yes'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(dialogContext).pop();
-                          },
-                          child: Text('No'),
-                        )
-                      ],
-                    ),
-                  ),
-                );
-              },
-              itemCount: state.notes.length,
-              separatorBuilder: (BuildContext context, int index) => Divider(),
-            );
-          } else if (state is NotesListEmptyState) {
-            return Center(
-              child: Text(
-                'List is empty\nLets create a first note!',
-                textAlign: TextAlign.center,
-              ),
-            );
-          } else {
-            return Container();
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => NoteScreen(
-              onEditingFinished: (String body, String title) {
-                Navigator.of(context).pop();
-                _notesListCubit.addNote(title, body);
+              itemBuilder: (BuildContext itemContext) {
+                return [
+                  PopupMenuItem<String>(value: 'LogOut', child: Text('LogOut')),
+                  PopupMenuItem<String>(value: 'User', child: Text('Edit User')),
+                ];
               },
             ),
-          ),
+          ],
         ),
-        child: Icon(
-          Icons.add,
+        body: BlocConsumer<NotesListCubit, NotesListState>(
+          cubit: _notesListCubit,
+          listener: (newContext, state) {
+            if (state is NotesListFailedState) {
+              showDialog(
+                context: context,
+                builder: (dialogContext) {
+                  return AlertDialog(
+                    title: Text('Error'),
+                    content: Container(
+                      child: Text(state.error),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(dialogContext).pop();
+                        },
+                        child: Text('OK'),
+                      )
+                    ],
+                  );
+                },
+              );
+            }
+          },
+          builder: (newContext, state) {
+            if (state is NotesListLoadingState || state is NotesListInitialState) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is NotesListFailedState) {
+              return _buildTryAgain();
+            } else if (state is NotesListLoadedState) {
+              final filteredNotes = <Note>[]..addAll(state.notes);
+              if (_searchTextController.text.isNotEmpty) {
+                filteredNotes
+                    .removeWhere((element) => !element.title.contains(_searchTextController.text));
+              }
+              return Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: _searchTextController,
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                      ),
+                    ),
+                  ),
+                  ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext itemContext, int index) {
+                      final note = filteredNotes[index];
+                      return NoteItem(
+                        title: note.title,
+                        date: note.dateCreated,
+                        onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => NoteScreen(
+                              note: note,
+                              onEditingFinished: (String body, String title) {
+                                Navigator.of(context).pop();
+                                final newNote = Note(body, title, note.id);
+                                _notesListCubit.editNote(newNote);
+                              },
+                            ),
+                            fullscreenDialog: true,
+                          ),
+                        ),
+                        onLongTap: () => showDialog(
+                          context: context,
+                          builder: (dialogContext) => AlertDialog(
+                            title: Text('Deleting'),
+                            content: Text('Do u want delete this note?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(dialogContext).pop();
+                                  _notesListCubit.deleteNote(note.id);
+                                },
+                                child: Text('Yes'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(dialogContext).pop();
+                                },
+                                child: Text('No'),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: filteredNotes.length,
+                    separatorBuilder: (BuildContext context, int index) => Divider(),
+                  ),
+                ],
+              );
+            } else if (state is NotesListEmptyState) {
+              return Center(
+                child: Text(
+                  'List is empty\nLets create a first note!',
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              return Container();
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => NoteScreen(
+                onEditingFinished: (String body, String title) {
+                  Navigator.of(context).pop();
+                  _notesListCubit.addNote(title, body);
+                },
+              ),
+            ),
+          ),
+          child: Icon(
+            Icons.add,
+          ),
         ),
       ),
     );
